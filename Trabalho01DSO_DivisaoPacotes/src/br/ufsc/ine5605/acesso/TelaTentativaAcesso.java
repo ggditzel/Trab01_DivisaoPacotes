d5194d33de888bc78b3d4d99cf7ef5e88fbe77c3
ggditzel@gmail.com <ggditzel@gmail.com>
2017-10-15 21:00:42 -0200

    Primeiro commit com a estrutura dividida em pacotes
    Retiradas importações desnecessárias

---
+++ Trabalho01DSO_DivisaoPacotes/src/br/ufsc/ine5605/acesso/TelaTentativaAcesso.java
@@ -0,0 +1,131 @@
+package br.ufsc.ine5605.acesso;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import br.ufsc.ine5605.Tela;
+
+public class TelaTentativaAcesso extends Tela {
+
+	public void mostraNegacao(MotivoNegacaoAcesso motivo) {
+		System.out.println(motivo.getNome());
+	}
+
+	public int mostraOpcoes() {
+		System.out.println("1- Usar horario e data local");
+		System.out.println("2- Definir horario e data (para testes)");
+		System.out.println("0- Voltar");
+		return super.leInteiroPositivoAte(2);
+	}
+
+	public String perguntaHora() {
+		String hora = "";
+		boolean respostaOK = false;
+		System.out.println("Digite o horario: ");
+		do{
+			hora = leitor.nextLine();
+			if(hora.matches("([01]?[0-9]|2[0-3]):[0-5][0-9]") ) {
+				respostaOK = true;
+				if(hora.charAt(1) == ':') {
+					hora = "0"+ hora;
+				}
+			} else {
+				System.out.println("Formato de hora invalido.\nDigite novamente");
+				respostaOK = false; // redundante
+			}
+		}while(!respostaOK);
+		
+		return hora;
+	}
+
+	public int perguntaMatricula() {
+		System.out.println("Digite sua matricula:"); 
+		return leInteiroPositivo();
+	}
+
+	public String perguntaData() {
+		String data = "";
+		boolean respostaOK = false;
+		System.out.println("Digite a data do acesso (dd/mm/aaaa): ");
+		do{
+			data = leitor.nextLine();
+			if(dataEhValida(data) ) {
+				respostaOK = true;
+				if(data.charAt(1) == '/') {
+					data = "0"+ data;
+				}
+				if(data.charAt(4) == '/') {
+					data = data.substring(0, 3) + "0" + data.substring(3);
+				}
+				if(data.length() != 10) {
+					respostaOK = false;
+					System.out.println("Formato de data invalido.\nDigite novamente");
+				}
+			} else {
+				System.out.println("Formato de data invalido.\nDigite novamente");
+				respostaOK = false; // redundante
+			}
+		}while(!respostaOK);
+		
+		return data;
+	}
+	
+	private boolean dataEhValida(String text) {
+		   
+	    SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+
+	    df.setLenient(false);
+	    try {
+	        df.parse(text);
+	        return true;
+	    } catch (ParseException ex) {
+	        return false;
+	    }
+	}
+
+	public void confirmaAcesso() {
+		System.out.println("Acesso permitido.");
+		
+	}
+
+	public int mostraMenuTentativas() {
+		System.out.println("Escolha uma opcao: ");
+		System.out.println(
+				"1- Listar todas as tentativas de acesso\n" +
+				"2- Listar todas as tentativas de acesso negadas\n" +
+				"3- Listar todos os acessos\n" +
+				"4- Listar todos as tentativas de acesso a partir de uma matricula\n" +
+				"5- Listar tentativas de acesso negadas a partir de uma matricula\n" +
+				"6- Listar os acessos a partir de uma matricula\n" +
+				"7- Listar tentativas de acesso negadas por um motivo\n" +
+				"0- Voltar"
+				);
+		
+
+		return leInteiroPositivoAte(7);
+	}
+
+	public void listaTentativas(ArrayList<TentativaAcesso> tentativas) {
+		if(tentativas.isEmpty()){
+			System.out.println("Nao ha tentativas de acesso");
+		} else {
+			for(TentativaAcesso t: tentativas){
+				System.out.println(t.toString());
+			}
+		}
+		System.out.println("Pressione enter para voltar");
+		leitor.nextLine();
+		
+	}
+
+	public int perguntaMotivo() {
+		int i = 0;
+		for(MotivoNegacaoAcesso m: MotivoNegacaoAcesso.values()) {
+			i++;
+			System.out.println(""+ i + "- " + m.getNome());
+		}
+		return leInteiroPositivoAte(i);
+	}
+	
+}
---
+++ Trabalho01DSO_DivisaoPacotes/src/br/ufsc/ine5605/horario/TelaHorario.java
@@ -0,0 +1,120 @@
+package br.ufsc.ine5605.horario;
+
+import java.util.ArrayList;
+
+import br.ufsc.ine5605.Tela;
+
+
+
+
+public class TelaHorario extends Tela{
+
+    
+    public TelaHorario(){
+        
+    }
+
+    /**
+	 * Solicita a digitacao de uma hora, no formato (hh:mm) ate que seja digitada corretamente
+	 * @return
+	 * 	String correspondente a hora digitada
+	 */
+    public String perguntaInicio() {
+        System.out.println("Insira o início do horario (hh:mm): ");
+        return leHora();
+    }
+    public String perguntaFim() {
+        System.out.println("Insira o fim do horario (hh:mm): ");
+        return leHora();
+    }
+
+	public int mostraOpcoesAdicionar() {
+			System.out.println("1- Adicionar horario de acesso \n"
+							 + "0- Voltar");
+		return super.leInteiroPositivoAte(1);
+	}
+	
+	public void listaHorarios(ArrayList<Horario> lista) {
+		System.out.println("Horarios cadastrados:");
+		for(int i = 0; i < lista.size(); i++){
+			System.out.println("" + (int) (i+1) + "- " + lista.get(i).toString());
+		}
+		System.out.println("");
+	}
+	
+	public void mostraLista(ArrayList<Horario> lista){
+		for(int i = 0; i < lista.size(); i++){
+			System.out.println("" + (int) (i+1) + "- " + lista.get(i).toString());
+		}
+	}
+	
+	public int mostraListaRemover(ArrayList<Horario> lista) {
+		System.out.println("Selecione um horario:");
+		mostraLista(lista);
+		System.out.println("0- Voltar");
+		return super.leInteiroPositivoAte(lista.size() + 1);
+	}
+	
+	public int mostraListaAdicionar(ArrayList<Horario> lista) {
+		System.out.println("Selecione um horario:");
+		mostraLista(lista);
+		System.out.println("" + (int) (lista.size() + 1) + "- Adicionar novo horario \n" +
+				  "0- Voltar");
+		return super.leInteiroPositivoAte(lista.size()+1);
+	}
+	
+	
+	
+	/**
+	 * Solicita a digitacao de uma hora, no formato (hh:mm) ate que seja digitada corretamente
+	 * @return
+	 * 	String correspondente a hora digitada
+	 */
+	private String leHora() {
+		String hora = "";
+		boolean respostaOK = false;
+		do{
+			hora = leitor.nextLine();
+			if(hora.matches("([01]?[0-9]|2[0-3]):[0-5][0-9]") ) {
+				respostaOK = true;
+				if(hora.charAt(1) == ':') {
+					hora = "0"+ hora;
+				}
+			} else {
+				System.out.println("Formato de hora invalido.\nDigite novamente");
+				respostaOK = false; // redundante
+			}
+		}while(!respostaOK);
+		
+		return hora;
+	}
+
+	/**
+	 * Imprime um aviso para o usuario
+	 * @param aviso
+	 * 	mensagem que sera impressa
+	 */
+	public void mostraAviso(String aviso) {
+		System.out.println(aviso);
+	}
+
+	public int mostraOpcoesEditar() {
+		System.out.println("1- Adicionar horario de acesso \n"
+				 + "2- Remover horario de acesso \n"
+				 + "0- Voltar");
+		return super.leInteiroPositivoAte(2);
+	}
+
+	public int mostraListaAdicionarPrimeiroHorario(
+			ArrayList<Horario> lista) {
+		System.out.println("Selecione um horario:");
+		mostraLista(lista);
+		System.out.println("0- Adicionar novo horario \n");
+		return super.leInteiroPositivoAte(lista.size());
+	}
+
+	
+	
+
+	
+}
